9fe93b698cb7b61aa768f32933f392964636067a
Olena Bardyn bc2025-7 <olenabardyn3@gmail.com>
2026-05-04 17:16:43 +0300

    Added test data

---
+++ trainBooking/src/data/trains.js
@@ -0,0 +1,52 @@
+export const trains = [
+  {
+    id: 1,
+    number: "128",
+    from: "Запоріжжя",
+    to: "Львів",
+    date: "20.05.2026",
+    time: "08:00",
+    duration: "5 год 30 хв",
+    price: 450
+  },
+  {
+    id: 2,
+    number: "724",
+    from: "Київ",
+    to: "Харків",
+    date: "21.05.2026",
+    time: "09:15",
+    duration: "8 год 45 хв",
+    price: 680
+  },
+  {
+    id: 3,
+    number: "080",
+    from: "Львів",
+    to: "Харків",
+    date: "21.05.2026",
+    time: "07:30",
+    duration: "4 год 30 хв",
+    price: 520
+  },
+  {
+    id: 4,
+    number: "312",
+    from: "Львів",
+    to: "Одеса",
+    date: "20.05.2026",
+    time: "14:15",
+    duration: "10 год 00 хв",
+    price: 890
+  },
+  {
+    id: 5,
+    number: "801",
+    from: "Львів",
+    to: "Запоріжжя",
+    date: "20.05.2026",
+    time: "16:45",
+    duration: "5 год 30 хв",
+    price: 320
+  }
+];
